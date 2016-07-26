3cde7c149e7827a4860035a5353e124758c034eb
Ric Harvey <ric@ngd.io>
2016-07-26 15:04:37 +0100

    pushing scripts

--- scripts/letsencrypt-setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Lets Encrypt
-if [ -z "$WEBROOT" ] || [ -z "$GIT_EMAIL" ] || [ -z "$DOMAIN" ]; then
- echo "You need the \$WEBROOT, \$GIT_EMAIL and the \$DOMAIN Variables"
-else
- /opt/letsencrypt/letsencrypt-auto certonly -a webroot --webroot-path=$WEBROOT --email $GIT_EMAIL -d $DOMAIN 
-fi
